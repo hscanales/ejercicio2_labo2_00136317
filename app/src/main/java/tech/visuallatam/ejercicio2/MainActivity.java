1c7e124e1151104ddecf4f031b71a1d36266ba4e
hscanales <00136317@uca.edu.sv>
2019-03-16 09:45:56 -0600

    BMF

--- app/src/main/java/tech/visuallatam/ejercicio2/MainActivity.java
+++ app/src/main/java/tech/visuallatam/ejercicio2/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView col1row2;
     ImageView col2row2;
 
+    TextView debug;
     Drawable[] imagenes;
 
 
@@ -48,9 +50,11 @@ public class MainActivity extends AppCompatActivity {
         col1row2 = findViewById(R.id.col1row2);
         col2row2 = findViewById(R.id.col2row2);
 
-        imagenes[0] = col0row0.getDrawable();
-        imagenes[1] = col1row0.getDrawable();
-        imagenes[2] = col2row0.getDrawable();
+        debug = findViewById(R.id.texto);
+        imagenes = new Drawable[3];
+        imagenes[0] = getResources().getDrawable(R.drawable.asda);
+        imagenes[1] = getResources().getDrawable(R.drawable.dz);
+        imagenes[2] = getResources().getDrawable(R.drawable.draw);
 
     }
 
@@ -59,9 +63,83 @@ public class MainActivity extends AppCompatActivity {
         col0row0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
-                col0row0.setImageDrawable();
+                int numero = (int)(Math.random()*3);
+                col0row0.setImageDrawable(imagenes[numero]);
+
             }
         });
+
+        col1row0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numero = (int)(Math.random()*3);
+                col1row0.setImageDrawable(imagenes[numero]);
+
+            }
+        });
+
+        col2row0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numero = (int)(Math.random()*3);
+                col2row0.setImageDrawable(imagenes[numero]);
+
+            }
+        });
+
+        col0row1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numero = (int)(Math.random()*3);
+                col0row1.setImageDrawable(imagenes[numero]);
+
+            }
+        });
+
+        col1row1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numero = (int)(Math.random()*3);
+                col1row1.setImageDrawable(imagenes[numero]);
+
+            }
+        });
+
+        col2row1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numero = (int)(Math.random()*3);
+                col2row1.setImageDrawable(imagenes[numero]);
+
+            }
+        });
+
+        col0row2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numero = (int)(Math.random()*3);
+                col0row2.setImageDrawable(imagenes[numero]);
+
+            }
+        });
+
+        col1row2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numero = (int)(Math.random()*3);
+                col1row2.setImageDrawable(imagenes[numero]);
+
+            }
+        });
+
+        col2row2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numero = (int)(Math.random()*3);
+                col2row2.setImageDrawable(imagenes[numero]);
+
+            }
+        });
+
     }
 }
